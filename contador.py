b712ebe9f2ca72016a676da97248673359a3a74d
JuanMartinMarina <58919693+JuanMartinMarina@users.noreply.github.com>
2023-05-23 16:31:01 -0300

    Update contador.py

--- contador.py
+++ contador.py
@@ -1,16 +1,16 @@
 import datetime
 import tkinter as tk
 
-# Crear ventana
+##Ventana
 window = tk.Tk()
 window.title("CONTADOR PARA IRME A LA MIERDA")
 
-# Hora
+##Hora
 now = datetime.datetime.now()
 target_hour = datetime.datetime(now.year, now.month, now.day, 14, 00, 00)
 time_left = (target_hour - now).total_seconds()
 
-# Función para actualizar el contador
+##Actualizar el contador
 def update_counter():
     global time_left
     if time_left > 0:
@@ -24,12 +24,12 @@ def update_counter():
         # Mostrar un mensaje cuando el tiempo se acaba
         counter_label.config(text="ANDATE A LA COMBI!")
 
-# Crear etiqueta del contador
+##Etiqueta del contador
 counter_label = tk.Label(window, font=("Arial", 60), fg="red")
 counter_label.pack(padx=50, pady=50)
 
-# Llamar a la función de actualización del contador
+##Llamar a la función de actualización del contador
 update_counter()
 
-# Mostrar ventana
+##Mostrar ventana
 window.mainloop()
